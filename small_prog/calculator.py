f8457ebd364e4cecaeb29e2de61a2e3c2c604c14
Luna Tek <34731676+luunatek@users.noreply.github.com>
2020-11-18 10:44:04 -0600

    Create calculator.py

---
+++ small_prog/calculator.py
@@ -0,0 +1,35 @@
+def add(x, y):
+    return x + y
+
+def subract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("Select an operation: ")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+choice = input("Enter a choice '1/2/3/4': ")
+
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter a second number: "))
+if choice == '1':
+    print(num1,'+',num2,'=', add(num1,num2))
+
+elif choice == '2':
+    print(num1, '-', num2, '=', subtract(num1,num2))
+
+elif choice == '3':
+    print(num1, '*', num2, '=', multiply(num1,num2))
+
+elif choice == '4':
+    print(num1, '/', num2, '=', divide(num1,num2))
+
+else:
+    print('Invalid input')
