5e5fe4b25d2e4f181a26a69db5a9037c6076a993
Luna Tek <34731676+luunatek@users.noreply.github.com>
2020-11-18 10:46:45 -0600

    Create file

---
+++ small_prog/pozitive_neg_num.py
@@ -0,0 +1,12 @@
+# Positive or negative number
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("The number is zero")
+elif num < 0:
+    print("Negative number" )
+else:
+    print("Invalid input")
